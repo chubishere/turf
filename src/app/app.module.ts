65479b7d5e5e9628f739b240a239dfcbf32fd146
chubishere <clarence@dubishere.com>
2017-03-27 00:40:24 +0100

    add local notifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { Camera } from '@ionic-native/camera';
 import { File } from '@ionic-native/file';
+import { LocalNotifications } from '@ionic-native/local-notifications';
 
 @NgModule({
   declarations: [
@@ -36,6 +37,7 @@ import { File } from '@ionic-native/file';
     SplashScreen,
     Camera,
     File,
+    LocalNotifications,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,7 @@ import { Component, NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 import { File } from '@ionic-native/file';
+import { LocalNotifications, ILocalNotification } from '@ionic-native/local-notifications';
 
 @Component({
   selector: 'page-home',
@@ -13,12 +14,15 @@ export class HomePage {
   image: string = '';
   files: string[] = [];
   counter = 0;
+  notificationCountdown: number = 3;
+  notificationClicked: boolean = false;
 
   constructor(
     public navCtrl: NavController,
     private camera: Camera,
-    private file:File,
-    private zone:NgZone
+    private file: File,
+    private lono: LocalNotifications,
+    private zone: NgZone
   ) {}
 
   onCamera() {
@@ -58,6 +62,32 @@ export class HomePage {
       });
       this.onFileList();
     });
+  }
+
+  onLocalNotify() {
+    this.notificationClicked = false;
+    let originalNotificationCountdown = this.notificationCountdown;
+    let interval = setInterval(() => {
+        this.zone.run(() => {
+          if( --this.notificationCountdown < 0 ) {
+            clearInterval( interval );
+            this.notificationCountdown = originalNotificationCountdown;
+          }
+        })
+      },
+      1000
+    );
+    const options: ILocalNotification = {
+      id: 42,
+      title: 'Title',
+      text: 'Text',
+      at: new Date(new Date().getTime() + 3 * 1000)
+    }
+    this.lono.schedule(options);
+    this.lono.on('click', () => {
+      console.log('clicko');
+      this.zone.run(() => this.notificationClicked = true);
+    })
   }
 
 }
